2e750eacbf2a21202d714ee88737df187d62a190
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-05-14 19:48:58 +0700

    push 14.5.2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'CNedu | Trang chủ',
+}
+
 export default function Home() {
   return (
-    <>
+    <div className="pt-[60px]">
       <h1 className="mt-[20px] text-center text-[34px] font-bold text-[#FF883E]">
         CNedu - Hệ thống giáo dục toàn diện
       </h1>
       <p className="text-center">Đang được phát triển bởi <strong>Lý Cao Nguyên</strong></p>
-    </>
+    </div>
   );
 }
