8bb8827723731dae630b716bbd7ae0e6b6410775
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-05-14 17:26:25 +0700

    push

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="mt-[20px] text-center text-[34px] font-bold text-[#FF883E]">
+        CNedu - Hệ thống giáo dục toàn diện
+      </h1>
+      <p className="text-center">Đang được phát triển bởi <strong>Lý Cao Nguyên</strong></p>
+    </>
+  );
+}
