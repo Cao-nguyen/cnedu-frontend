d02deef578c6fc63d4155e351b185a7973614f69
Cao-nguyen <chinhphucnguvan@gmail.com>
2025-05-14 19:55:51 +0700

    push 14.5.2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,15 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="pt-[60px]">
-      <h1 className="mt-[20px] text-center text-[34px] font-bold text-[#FF883E]">
-        CNedu - Hệ thống giáo dục toàn diện
+      <h1 className="
+        mx-[20px]
+        mt-[20px]
+        text-center
+        text-[40px]
+        font-bold
+        text-[#FF883E]"
+      >
+        CNedu - Hệ thống giáo dục toàn diện được phát triển bởi Lý Cao Nguyên
       </h1>
       <p className="text-center">Đang được phát triển bởi <strong>Lý Cao Nguyên</strong></p>
     </div>
